72744c097025334330e75b359332d002592ed434
Baha Büçge <bahabucge@gmail.com>
2022-01-03 21:50:52 +0300

    migration for seen abilty of notf

---
+++ AKAN/Migrations/20220103185022_NotificationSeenDataAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AKAN.Migrations
+{
+    public partial class NotificationSeenDataAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Seen",
+                table: "Notifications",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seen",
+                table: "Notifications");
+        }
+    }
+}
