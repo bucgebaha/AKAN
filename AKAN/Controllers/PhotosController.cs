1cc76a7f098d9276433d44c2859919442f662ba8
Baha Büçge <bahabucge@gmail.com>
2022-01-13 20:38:31 +0300

    photos v1

--- AKAN/Controllers/PhotosController.cs
+++ AKAN/Controllers/PhotosController.cs
@@ -11,7 +11,7 @@ using AKAN.Models;
 
 namespace AKAN.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class PhotosController : ControllerBase
     {
